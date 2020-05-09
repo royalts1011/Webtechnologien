cb81125616fe0b7704d3ae1d80161e6b777af49b
Falco Lentzsch <falco.lentzsch@student.uni-luebeck.de>
2020-05-09 13:35:37 +0200

    aufgabe1 fertig

---
+++ Aufgabe_2/ShadowDomScript.js
@@ -0,0 +1,14 @@
+customElements.define('title', AbgabeButton);
+
+class AbgabeButton extends HTMLElement {
+    constructor() {
+        //Super muss immer zuerst aufgerufen werden
+        super();
+        // Element-Funktionalität wird ab hier definiert
+        const shadow = this.attachShadow({
+            mode: "open"
+        });
+    }
+
+    this.getAttribute("imgSrc) 
+}
